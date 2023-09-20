9bc8e9fd03a6e151c0864eb949489e3d23d98856
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-09-20 15:20:01 +0100

    initialize backend

---
+++ capstone_backend/src/main/java/com/bnta/capstone_backend/CapstoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.capstone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneBackendApplication.class, args);
+    }
+
+}
