5b0dab81775a528a882634539bc3a7247c2115d4
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-09-21 10:40:11 +0100

    initiated modal

--- capstone_frontend/src/Components/LandingPage.js
+++ capstone_frontend/src/Components/LandingPage.js
@@ -1,11 +1,30 @@
 import CategoriesBox from "./CategoriesBox";
 import NavBar from "./NavBar";
+import "./LandingPage.css"
+
 
 const LandingPage = () => {
 
     return(
         <div>
             <NavBar />
+            {/* <div className="body">
+                <div className="container">
+                    <div className="carousel">
+                        <figure>
+                            <img src="https://images.unsplash.com/photo-1501420896719-ad7fe0ee297e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1629732046253-e9c2641f7cd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1599661194055-63427c8e6e70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2904&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3001&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1620331313174-3c6cfd5e292a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1620799140188-3b2a02fd9a77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2872&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1602268394804-6a31d350d862?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2874&q=80"/>
+                            <img src="https://images.unsplash.com/photo-1625093525885-282384697917?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2901&q=80"/>
+                        </figure>
+                    </div>
+                </div>
+            </div> */}
+
             <CategoriesBox />
         </div>
     )
--- capstone_frontend/src/Components/NavBar.js
+++ capstone_frontend/src/Components/NavBar.js
@@ -1,14 +1,42 @@
 import { useNavigate } from "react-router-dom";
+import "./NavBar.css"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const NavBar = () => {
     const navigate = useNavigate();
 
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return(
-        <div>
+        <div className="nav-bar">
             <button onClick={() => navigate("/")}>Logo</button>
             <button onClick={() => navigate("/Products")}>All Products</button>
             <button onClick={() => navigate("/LogIn")}>Order History</button>
-            <button >Basket</button>
+            <Button id="modal-button" variant= "contained" onClick={handleOpen}>Basket</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box >
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Who is?
+                </Typography>
+                {/* <Typography id="petmodal-image">
+                    <img src={`pet_images/pet_${pet.species}3.gif`} />
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {displayMessage()}
+                </Typography> */}
+                </Box>
+            </Modal>
         </div>
     )
 }
