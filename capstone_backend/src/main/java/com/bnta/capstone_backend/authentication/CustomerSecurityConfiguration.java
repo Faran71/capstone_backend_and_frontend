1d0e57e53265ab0099ebfc18c1aa89164ebb95c6
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-09-25 15:32:50 +0100

    auth push

---
+++ capstone_backend/src/main/java/com/bnta/capstone_backend/authentication/CustomerSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.bnta.capstone_backend.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class CustomerSecurityConfiguration{
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+
+}
