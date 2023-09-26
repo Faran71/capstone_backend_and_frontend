5271746c5c8caa81b47ebcc8e7e723c9bf1e53d7
essamcreates <mrm.hussain@hotmail.co.uk>
2023-09-26 13:03:56 +0100

    added carousel feature

--- capstone_frontend/src/Components/LandingPage.js
+++ capstone_frontend/src/Components/LandingPage.js
@@ -2,20 +2,21 @@ import CategoriesBox from "./CategoriesBox";
 import NavBar from "./NavBar";
 import "./LandingPage.css";
 import Carousel from "./Carousel";
+import images from "../images";
 
 
 
 const LandingPage = ({order, products, setOrder, currentCustomer, setCurrentCustomer}) => {
-    const images=[
-                "https://images.unsplash.com/photo-1501420896719-ad7fe0ee297e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
-                "https://images.unsplash.com/photo-1629732046253-e9c2641f7cd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
-                "https://images.unsplash.com/photo-1599661194055-63427c8e6e70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2904&q=80",
-                "https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3001&q=80",
-                "https://images.unsplash.com/photo-1620331313174-3c6cfd5e292a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
-                "https://images.unsplash.com/photo-1620799140188-3b2a02fd9a77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2872&q=80",
-                "https://images.unsplash.com/photo-1602268394804-6a31d350d862?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2874&q=80",
-                "https://images.unsplash.com/photo-1625093525885-282384697917?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2901&q=80"
-            ]
+    // const images=[
+    //             "https://images.unsplash.com/photo-1501420896719-ad7fe0ee297e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+    //             "https://images.unsplash.com/photo-1629732046253-e9c2641f7cd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+    //             "https://images.unsplash.com/photo-1599661194055-63427c8e6e70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2904&q=80",
+    //             "https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3001&q=80",
+    //             "https://images.unsplash.com/photo-1620331313174-3c6cfd5e292a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+    //             "https://images.unsplash.com/photo-1620799140188-3b2a02fd9a77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2872&q=80",
+    //             "https://images.unsplash.com/photo-1602268394804-6a31d350d862?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2874&q=80",
+    //             "https://images.unsplash.com/photo-1625093525885-282384697917?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2901&q=80"
+    //         ]
 
     return(
         <div>
