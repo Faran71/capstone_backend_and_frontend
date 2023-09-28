c40f40e8532b915a07d7b77ed9caa3db5bd3ba9e
Abdulaziz-MI <am.alzaid@outlook.com>
2023-09-28 13:31:56 +0300

    set up review controller core and service logic

--- capstone_backend/src/main/java/com/bnta/capstone_backend/controllers/ReviewController.java
+++ capstone_backend/src/main/java/com/bnta/capstone_backend/controllers/ReviewController.java
@@ -1,5 +1,12 @@
 package com.bnta.capstone_backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping({"/reviews"})
 public class ReviewController {
+
+
 }
--- capstone_backend/src/main/java/com/bnta/capstone_backend/services/ReviewService.java
+++ capstone_backend/src/main/java/com/bnta/capstone_backend/services/ReviewService.java
@@ -1,5 +1,27 @@
 package com.bnta.capstone_backend.services;
 
+import com.bnta.capstone_backend.repositories.ProductRepository;
+import com.bnta.capstone_backend.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+// GET product by ID
+//    Create method that takes in customer name, review msg and product id from DTO
+//    method will retrieve product  by its id from the frontend
+//    save to the review table
+
+
+
+
+
 
 }
