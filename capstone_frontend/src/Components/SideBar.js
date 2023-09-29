584e8d8b78191cacb566f318a95066a17f908c62
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-09-29 09:34:07 +0100

    slight change of position od slidebar

--- capstone_frontend/src/Components/SideBar.js
+++ capstone_frontend/src/Components/SideBar.js
@@ -84,7 +84,7 @@ const handlePriceFilter = () => {
                 <div>
                     <h3>Price Range</h3>
                     <div className="slider">
-                        <Box sx={{ width: 200 }}>
+                        <Box sx={{ width: 170 }}>
                         <Slider
                             getAriaLabel={() => 'Temperature range'}
                             value={value}
